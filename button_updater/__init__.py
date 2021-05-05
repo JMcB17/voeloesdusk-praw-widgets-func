67505446c6beec5c71a88e807e06fd7b3e56cee5
JMcB <joel.mcbride1@live.com>
2021-05-05 19:23:03 +0100

    Fix bugs, raise KeyError if search term not found

--- button_updater/__init__.py
+++ button_updater/__init__.py
@@ -4,15 +4,17 @@ import asyncpraw.models
 __version__ = '1.0.0'
 
 
-# todo: handle if search term not found
+# todo: add argument to make it case sensitive
+# todo: write docstrings
 async def find_button_widget(subreddit: asyncpraw.models.Subreddit, name: str) -> asyncpraw.models.ButtonWidget:
-    all_widgets = await subreddit.widgets()
-    widgets_dict = all_widgets.items()
+    widgets_dict = await subreddit.widgets.items()
     button_widgets = [w for w in widgets_dict.values() if isinstance(w, asyncpraw.models.ButtonWidget)]
 
     for widget in button_widgets:
         if widget.shortName.casefold() == name.casefold():
             return widget
+
+    raise KeyError(f'Button widget named {name} not found in subreddit {subreddit.name}')
 
 
 async def find_button(button_widget: asyncpraw.models.ButtonWidget, name: str) -> asyncpraw.models.Button:
@@ -20,8 +22,10 @@ async def find_button(button_widget: asyncpraw.models.ButtonWidget, name: str) -
         if button.text.casefold() == name.casefold():
             return button
 
+    raise KeyError(f'Button named {name} not found in button widget {button_widget.shortName}')
 
-def update_button(
+
+async def update_button(
         subreddit: asyncpraw.models.Subreddit,
         button_widget_name: str, button_name: str,
         new_button_text: str, new_button_url: str
@@ -30,6 +34,9 @@ def update_button(
     button = await find_button(button_widget, button_name)
 
     # todo: check if this works or if I have to do it with button_widget.mod.update()
+    # it doesn't work, need to use a different method
+    # raises asyncprawcore.exceptions.Forbidden http 403 if not mod
+    # also if sub is private
     button.text = new_button_text
     button.url = new_button_url
 
