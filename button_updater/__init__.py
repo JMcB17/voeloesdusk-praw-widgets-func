49717fa5e47660122c64eeae6365f8e764ccbedf
JMcB <joel.mcbride1@live.com>
2021-05-05 18:16:04 +0100

    Create files

---
+++ button_updater/__init__.py
@@ -0,0 +1,8 @@
+import asyncpraw
+
+
+__version__ = '1.0.0'
+
+
+def update_button():
+    pass
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import button_updater
+
+
+with open('README.md', encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
+setuptools.setup(
+    name='button-updater',
+    version=button_updater.__version__,
+    author='JMcB',
+    author_email='joel.mcbride1@live.com',
+    license='MIT',
+    description='Fiver commission for voeloesdusk',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/JMcB17/voeloesdusk-praw-widgets-func',
+    packages=setuptools.find_packages(),
+    python_requires='>=3',
+    install_requires=[
+        'asyncpraw',
+    ]
+)
