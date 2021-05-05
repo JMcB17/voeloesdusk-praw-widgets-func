a3b1a3fa7eb52ebe7e2bec61a243e3ba08881fee
JMcB <joel.mcbride1@live.com>
2021-05-05 18:47:51 +0100

    Initial code, needs testing and fixing

--- button_updater/__init__.py
+++ button_updater/__init__.py
@@ -1,8 +1,35 @@
-import asyncpraw
+import asyncpraw.models
 
 
 __version__ = '1.0.0'
 
 
-def update_button():
-    pass
+# todo: handle if search term not found
+async def find_button_widget(subreddit: asyncpraw.models.Subreddit, name: str) -> asyncpraw.models.ButtonWidget:
+    all_widgets = await subreddit.widgets()
+    widgets_dict = all_widgets.items()
+    button_widgets = [w for w in widgets_dict.values() if isinstance(w, asyncpraw.models.ButtonWidget)]
+
+    for widget in button_widgets:
+        if widget.shortName.casefold() == name.casefold():
+            return widget
+
+
+async def find_button(button_widget: asyncpraw.models.ButtonWidget, name: str) -> asyncpraw.models.Button:
+    for button in button_widget:
+        if button.text.casefold() == name.casefold():
+            return button
+
+
+def update_button(
+        subreddit: asyncpraw.models.Subreddit,
+        button_widget_name: str, button_name: str,
+        new_button_text: str, new_button_url: str
+):
+    button_widget = await find_button_widget(subreddit, button_widget_name)
+    button = await find_button(button_widget, button_name)
+
+    # todo: check if this works or if I have to do it with button_widget.mod.update()
+    button.text = new_button_text
+    button.url = new_button_url
+
